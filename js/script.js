75eafc66cb2a0a68e6672fe391c77a8672720671
Vrybalcenko <165685927+Vrybalcenko@users.noreply.github.com>
2024-12-19 00:04:46 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,123 @@
+// Якщо змінна більше нуля - виведіть true, менше - false
+//Перевірте це на варіантах 1, 0, -3.
+
+// const number = -3
+
+// if (number > 0) {
+//     console.log("true")
+// } else (number < 0); {
+//     console.log("false")
+// }
+
+
+
+// Якщо змінна ="test" - виведіть true,
+//Перевірте це на варіантах 'test',"qwerty", true
+
+// let word = "test"
+
+// if (word === "test") {
+//     console.log("true")
+// } else; {
+//     console.log("false")
+// }
+
+
+
+// Якщо змінна більше 10 - відніміть 5,
+//менше - додайте 5, результат виведіть в консоль
+//Перевірте це на варіантах 1, 10, 13.
+
+// const number = "13"
+
+// if (Number (number) >= 10) {
+//     console.log(Number (number) - 5)
+// } else {
+//     console.log(Number (number) + 5)
+// }
+
+//Зробіть сервіс який отримує число від 1 до 12
+// виведіть місяць який дорівнює числу
+
+// let value = prompt("Введіть число від 1 до 12");
+
+// switch (value) {
+//     case 1:
+//         value = "січень"
+//         alert(`${value}`)
+//         break;
+//     case 2:
+//         value = "лютий"
+//         alert(`${value}`)
+//         break;
+
+//     case 3:
+//         value = "березень"
+//         alert(`${value}`)
+//         break;
+
+//     case 4:
+//         value = "квітень"
+//         alert(`${value}`)
+//         break;
+
+//     case 5:
+//         value = "травень"
+//         alert(`${value}`)
+//         break;
+
+//     case 6:
+//         value = "червень"
+//         alert(`${value}`)
+//         break;
+
+//     case 7:
+//         value = "липень"
+//         alert(`${value}`)
+//         break;
+
+//     case 8:
+//         value = "серпень"
+//         alert(`${value}`)
+//         break;
+
+//     case 9:
+//         value = "вересень"
+//         alert(`${value}`)
+//         break;
+
+//     case 10:
+//         value = "жовтень"
+//         alert(`${value}`)
+//         break;
+
+//     case 11:
+//         value = "листопад"
+//         alert(`${value}`)
+//         break;
+    
+//     case 12:
+//         value = "грудень"
+//         alert(`${value}`)
+//         break;
+// }
+
+//Зробіть сервіс який отримує тризначне число
+//Поверніть користувачу сумму цих чисел
+
+
+// const digit = prompt("Введіть будь-яке тризначне число")
+// let sum = 0;
+
+//     if (digit > 100 && digit < 999) {
+//     const digitsArray = digit.split('');
+//     sum = digitsArray.reduce((acc, digit) => acc + parseInt(digit), 0);
+//     alert(`Сума цифр числа: ${sum}`)
+// } else {
+//     alert("Будь ласка, введіть коректне тризначне число.");
+// }
+
+
+
+
+
